aae511ba650451d892261b7ff76d55f340d241bd
araika06 <156989819+araika06@users.noreply.github.com>
2025-03-23 22:05:34 +0500

    Add files via upload

---
+++ Lab7/informatics/while/a.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+square = 1
+num = 1
+
+while square<=n:
+    print(square)
+    num += 1
+    square = num * num
---
+++ Lab7/informatics/while/e.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+power = 1
+i = 0
+
+while power < n:
+    power *= 2
+    i += 1
+
+print(i)
